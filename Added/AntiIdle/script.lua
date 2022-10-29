918fd742690f575d88a5655a875d573cdbfcd3ce
ㅤㅤㅤ <64054647+catgirIz@users.noreply.github.com>
2022-10-28 22:10:12 -0400

    Update script.lua

--- Added/AntiIdle/script.lua
+++ Added/AntiIdle/script.lua
@@ -1,1 +1,6 @@
-print("This is just an example");
+	local vu = game:GetService("VirtualUser")
+	game:GetService("Players").LocalPlayer.Idled:connect(function()
+		vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		wait(1)
+		vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+	end)
